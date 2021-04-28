0beec7caa692af3eff01a6cdb8d38154757c5554
Rafal <rafal.szczuka05@gmail.com>
2021-04-28 19:22:49 +0200

    End of section 19

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -10,6 +10,7 @@ import {
   IBasketTotals,
 } from '../shared/models/basket';
 import { IProduct } from '../shared/models/product';
+import { IDeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root',
@@ -20,8 +21,14 @@ export class BasketService {
   basket$ = this.basketSource.asObservable();
   private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
   basketTotal$ = this.basketTotalSource.asObservable();
+  shipping = 0;
 
   constructor(private http: HttpClient) {}
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod) {
+    this.shipping = deliveryMethod.price;
+    this.calculateTotals();
+  }
 
   getBasket(id: string) {
     return this.http.get(this.baseUrl + 'basket?id=' + id).pipe(
@@ -101,6 +108,12 @@ export class BasketService {
     );
   }
 
+  deleteLocalBasket(id: string) {
+    this.basketSource.next(null);
+    this.basketTotalSource.next(null);
+    localStorage.removeItem('basket_id');
+  }
+
   private addOrUpdateItem(
     items: IBasketItem[],
     itemToAdd: IBasketItem,
@@ -141,7 +154,7 @@ export class BasketService {
 
   private calculateTotals() {
     const basket = this.getCurrentBasketValue();
-    const shipping = 0;
+    const shipping = this.shipping;
     const subtotal = basket.items.reduce((a, b) => b.price * b.quantity + a, 0);
     const total = subtotal + shipping;
     this.basketTotalSource.next({ shipping, total, subtotal });
---
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -0,0 +1,56 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { BasketService } from '../../basket/basket.service';
+import { CheckoutService } from '../checkout.service';
+import { ToastrService } from 'ngx-toastr';
+import { IBasket } from 'src/app/shared/models/basket';
+import { IOrderToCreate, IOrder } from '../../shared/models/order';
+import { Router, NavigationExtras } from '@angular/router';
+
+@Component({
+  selector: 'app-checkout-payment',
+  templateUrl: './checkout-payment.component.html',
+  styleUrls: ['./checkout-payment.component.scss'],
+})
+export class CheckoutPaymentComponent implements OnInit {
+  @Input() checkoutForm: FormGroup;
+
+  constructor(
+    private basketService: BasketService,
+    private checkoutService: CheckoutService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  submitOrder() {
+    const basket = this.basketService.getCurrentBasketValue();
+    const orderToCreate: IOrderToCreate = this.getOrderToCreate(basket);
+    this.checkoutService.createOrder(orderToCreate).subscribe(
+      (order: IOrder) => {
+        this.toastr.success('Order created successfully');
+        this.basketService.deleteLocalBasket(basket.id);
+
+        const navExtras: NavigationExtras = {
+          state: order,
+        };
+        this.router.navigate(['checkout/success'], navExtras);
+      },
+      (err) => {
+        this.toastr.error(err.message);
+        console.log(err);
+      }
+    );
+  }
+
+  private getOrderToCreate(basket: IBasket): IOrderToCreate {
+    return {
+      basketId: basket.id,
+      deliveryMethodId: +this.checkoutForm
+        .get('deliveryForm')
+        .get('deliveryMethod').value,
+      shipToAddress: this.checkoutForm.get('addressForm').value,
+    };
+  }
+}
